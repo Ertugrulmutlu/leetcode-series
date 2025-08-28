c77946edc7e05354688aa3aa8086eaeefd519d25
Ertugrul Mutlu <65208049+Ertugrulmutlu@users.noreply.github.com>
2025-08-28 09:41:00 +0200

    Update Word Search.cpp

--- Day 12/Word Search.cpp
+++ Day 12/Word Search.cpp
@@ -1,23 +1,31 @@
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        m, n = len(board),len(board[0])
-        def dfs(r,c,i) ->bool:
-            if i == len(word):
-                return True
-            if r <0 or r >=m or c < 0 or c>=n or board[r][c] != word[i]:
-                return False
-            temp = board[r][c]
-            board[r][c] = '#'
-            found = (
-                dfs(r+1, c, i+1) or
-                dfs(r-1, c, i+1) or
-                dfs(r, c+1, i+1) or
-                dfs(r, c-1, i+1)
-            )
-            board[r][c] = temp
-            return found
-        for r in range(m):
-            for c in range(n):
-                if dfs(r,c,0):
-                    return True
-        return False
+using namespace std;
+
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size(), n = board[0].size();
+        vector<pair<int,int>> dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        function<bool(int,int,int)> dfs = [&](int r, int c, int i) {
+            if (i == word.size()) return true;
+            if (r < 0 || r >= m || c < 0 || c >= n || board[r][c] != word[i]) return false;
+            char tmp = board[r][c];
+            board[r][c] = '#';
+            for (auto [dr, dc] : dirs) {
+                if (dfs(r+dr, c+dc, i+1)) {
+                    board[r][c] = tmp;
+                    return true;
+                }
+            }
+            board[r][c] = tmp;
+            return false;
+        };
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (dfs(r, c, 0)) return true;
+            }
+        }
+        return false;
+    }
+};
