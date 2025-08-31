612b4f2d2c266166bfc0d657fe4a46e4db969773
Ertugrul Mutlu <65208049+Ertugrulmutlu@users.noreply.github.com>
2025-08-31 14:22:26 +0200

    Update Happy Number.cpp

---
+++ Day 15/Happy Number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+        while(n != 1 && seen.find(n) == seen.end()){
+            seen.insert(n);
+            n = nextNumber(n);
+        }
+        return n ==1;
+
+    }
+private:
+    int nextNumber(int n){
+        int total = 0;
+        while (n > 0){
+            int digit = n%10;
+            total += digit * digit;
+            n /=10;
+        }
+        return total;
+    }
+};
