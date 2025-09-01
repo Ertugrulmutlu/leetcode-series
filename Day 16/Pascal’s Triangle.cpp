10cfd8f40a9787910a88080fc1dce86027444e38
Ertugrul Mutlu <65208049+Ertugrulmutlu@users.noreply.github.com>
2025-09-01 12:29:24 +0200

    Update Pascal’s Triangle.cpp

---
+++ Day 16/Pascal’s Triangle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        result.push_back({1});
+
+        for (int i = 1; i < numRows; i++) {
+            vector<int> prev = result.back();
+            vector<int> row;
+            row.push_back(1);
+
+            for (int j = 1; j < prev.size(); j++) {
+                row.push_back(prev[j-1] + prev[j]);
+            }
+
+            row.push_back(1);
+            result.push_back(row);
+        }
+
+        return result;
+    }
+};
